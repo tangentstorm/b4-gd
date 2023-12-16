968f75dd05f7616b4ee488a050f291921955f390
tangentstorm <michal.wallace@gmail.com>
2023-12-16 03:58:02 -0500

    first batch of ops and cli commands

--- B4VM.gd
+++ B4VM.gd
@@ -1,9 +1,74 @@
-class_name B4VM extends Node
+extends Node
 
-var ds = PackedInt32Array()
-var cs = PackedInt32Array()
-var ram = PackedInt32Array()
-var ip = 0
+var ds = PackedInt32Array() # data stack
+var cs = PackedInt32Array() # call stack
+var ram = PackedByteArray() # ram
+var ip = 0x100
+
+
+func pop(ia:PackedInt32Array)->int:
+	if ia.size() == 0:
+		printerr("stack underflow")
+		return 0
+	else:
+		var res = ia[ia.size() - 1]
+		ia.resize(ia.size() - 1)
+		return res
+
+
+
+func tos(ia:PackedInt32Array)->int:
+	# "top of stack"
+	if ia.size() == 0:
+		printerr("stack underflow")
+		return 0
+	else:
+		return ia[ia.size() - 1]
+
+func nos(ia:PackedInt32Array)->int:
+	# "next on stack"
+	if ia.size() < 2:
+		printerr("stack underflow")
+		return 0
+	else:
+		return ia[ia.size() - 2]
+
+func dtos()->int: return tos(ds)
+func dnos()->int: return nos(ds)
+func ctos()->int: return tos(cs)
+func cpop()->int:	return pop(cs)
+func dpop()->int:	return pop(ds)
+
+
+func run_op(s:String)->bool:
+	match s:
+		"du": ds.push_back(dtos())
+		"dr": cs.push_back(dpop())
+		"rd": ds.push_back(cpop())
+		"sw": var a = dpop(); var b = dpop(); ds.push_back(a); ds.push_back(b)
+		"ov": ds.push_back(dnos())
+		"ad": var a = dpop(); var b = dpop(); ds.push_back(a + b)
+		"sb": var a = dpop(); var b = dpop(); ds.push_back(b - a)
+		"ml": var a = dpop(); var b = dpop(); ds.push_back(a * b)
+		"dv": var a = dpop(); var b = dpop(); ds.push_back(a / b)
+		"md": var a = dpop(); var b = dpop(); ds.push_back(a % b)
+		"ng": var a = dpop(); ds.push_back(-a)
+		"sl": var a = dpop(); var b = dpop(); ds.push_back(a << b)
+		"sr": var a = dpop(); var b = dpop(); ds.push_back(a >> b)
+		"an": var a = dpop(); var b = dpop(); ds.push_back(a & b)
+		"or": var a = dpop(); var b = dpop(); ds.push_back(a | b)
+		"xr": var a = dpop(); var b = dpop(); ds.push_back(a ^ b)
+		"nt": var a = dpop(); ds.push_back(~a)
+		"eq": var a = dpop(); var b = dpop(); ds.push_back(a == b)
+		"ne": var a = dpop(); var b = dpop(); ds.push_back(a != b)
+		"lt": var a = dpop(); var b = dpop(); ds.push_back(a < b)
+		"gt": var a = dpop(); var b = dpop(); ds.push_back(a > b)
+		"le": var a = dpop(); var b = dpop(); ds.push_back(a <= b)
+		"ge": var a = dpop(); var b = dpop(); ds.push_back(a >= b)
+		"zp": dpop() # "zap"
+		_: return false
+	return true
+
 
 func _init():
 	pass
--- cli.gd
+++ cli.gd
@@ -2,9 +2,42 @@ extends SceneTree
 
 const B4VM = preload("res://B4VM.gd")
 
+
+func make_input_stream():
+	# returns a function that gets a string
+	var args = OS.get_cmdline_user_args()
+	if args.size() > 0:
+		var f = FileAccess.open("res://"+args[0],FileAccess.READ)
+		return func():
+			return f.get_line().strip_edges()
+	else:
+		return func():
+			return OS.read_string_from_stdin().strip_edges()
+
+func hexdump(data:PackedInt32Array) -> String:
+	var toks = []
+	for n in data: toks.append('$%X' % n)
+	return '[' + ' '.join(toks) + ']'
+
 func _init():
-	print("hello")
+	var get_cmds = make_input_stream()
 	var vm = B4VM.new()
-	print(vm.ds)
+	var done = false
+	while not done:
+		var cmds = get_cmds.call().split(" ")
+		for cmd in cmds:
+			if done: break
+			match cmd:
+				"%q": done = true
+				"?d": print('ds: ', hexdump(vm.ds))
+				"?c": print('cs: ', hexdump(vm.cs))
+				"?i": print('ip: ', '$%X' % vm.ip)
+				_:
+					if vm.run_op(cmd): pass
+					elif cmd.is_valid_hex_number() and (cmd==cmd.to_upper()):
+						vm.ds.append(cmd.hex_to_int())
+					else:
+						print("!! what does '",cmd,"' mean?")
+						done = true
 	vm.free()
 	quit()
