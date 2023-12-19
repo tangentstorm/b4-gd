c5b340da17bdced27d42d9adb505ef2ebd6f084f
tangentstorm <michal.wallace@gmail.com>
2023-12-19 10:40:43 -0500

    B4VM: use dput(), and add todo() for some missing ops

--- B4VM.gd
+++ B4VM.gd
@@ -5,6 +5,15 @@ var cs = PackedInt32Array() # call stack
 var ram = PackedByteArray() # ram
 var ip = 0x100
 
+enum Op {
+	EX = 0x7F,
+	LB, LI, DU, SW, OV, ZP, DC, CD,
+	AD, SB, ML, DV, MD, SH,
+	AN, OR, XR, NT,
+	EQ, LT,
+	HP, H0, CL, RT,
+	IO, MV,
+	RB, WB, RI, WI, NX, DB }
 
 func pop(ia:PackedInt32Array)->int:
 	if ia.size() == 0:
@@ -15,7 +24,6 @@ func pop(ia:PackedInt32Array)->int:
 		ia.resize(ia.size() - 1)
 		return res
 
-
 func tos(ia:PackedInt32Array)->int:
 	# "top of stack"
 	if ia.size() == 0:
@@ -23,7 +31,6 @@ func tos(ia:PackedInt32Array)->int:
 		return 0
 	else:
 		return ia[ia.size() - 1]
-
 
 func nos(ia:PackedInt32Array)->int:
 	# "next on stack"
@@ -38,33 +45,41 @@ func dnos()->int: return nos(ds)
 func ctos()->int: return tos(cs)
 func cpop()->int:	return pop(cs)
 func dpop()->int:	return pop(ds)
+func dput(n:int): ds.push_back(n)
+func cput(n:int): cs.push_back(n)
 
+func todo(s:String): print("TODO: ", s)
 
 func run_op(s:String)->bool:
 	# TODO: map these to bytes and dispatch on those
 	match s:
 		"..": return true # no-op
-		"du": ds.push_back(dtos())
-		"dc": cs.push_back(dpop())
-		"cd": ds.push_back(cpop())
-		"sw": var a = dpop(); var b = dpop(); ds.push_back(a); ds.push_back(b)
-		"ov": ds.push_back(dnos())
-		"ad": var a = dpop(); var b = dpop(); ds.push_back(a + b)
-		"sb": var a = dpop(); var b = dpop(); ds.push_back(b - a)
-		"ml": var a = dpop(); var b = dpop(); ds.push_back(a * b)
-		"dv": var a = dpop(); var b = dpop(); ds.push_back(floor(b) / a)
-		"md": var a = dpop(); var b = dpop(); ds.push_back(b % a)
-		"sh": var a = dpop(); var b = dpop(); ds.push_back(b << a)
-		"an": var a = dpop(); var b = dpop(); ds.push_back(a & b)
-		"or": var a = dpop(); var b = dpop(); ds.push_back(a | b)
-		"xr": var a = dpop(); var b = dpop(); ds.push_back(a ^ b)
-		"nt": var a = dpop(); ds.push_back(~a)
-		"eq": var a = dpop(); var b = dpop(); ds.push_back(-int(b == a))
-		"lt": var a = dpop(); var b = dpop(); ds.push_back(-int(b < a))
+		"lb": dput(ram[ip]); ip += 1 # "load byte"
+		"li": todo("li")
+		"du": dput(dtos())
+		"ov": dput(dnos())
+		"sw": var a = dpop(); var b = dpop(); dput(a); dput(b)
 		"zp": dpop() # "zap"
+		"dc": cput(dpop())
+		"cd": dput(cpop())
+		"ad": var a = dpop(); var b = dpop(); dput(b + a)
+		"sb": var a = dpop(); var b = dpop(); dput(b - a)
+		"ml": var a = dpop(); var b = dpop(); dput(b * a)
+		"dv": var a = dpop(); var b = dpop(); dput(int(float(b)/a))
+		"md": var a = dpop(); var b = dpop(); dput(b % a)
+		"sh": var a = dpop(); var b = dpop(); dput(b << a)
+		"an": var a = dpop(); var b = dpop(); dput(b & a)
+		"or": var a = dpop(); var b = dpop(); dput(b | a)
+		"xr": var a = dpop(); var b = dpop(); dput(b ^ a)
+		"nt": var a = dpop(); dput(~a)
+		"eq": var a = dpop(); var b = dpop(); dput(-int(b == a))
+		"lt": var a = dpop(); var b = dpop(); dput(-int(b < a))
+		"hp": todo("hp")
+		"h0": todo("h0")
+		"cl": todo("cl")
+		"rt": ip = cpop()
 		"wb": var a = dpop(); var b = dpop(); ram[b] = a
-		"rb": ds.push_back(ram[dpop()])
-		"lb": ds.push_back(ram[ip]); ip += 1
+		"rb": dput(ram[dpop()])
 		_: return false
 	return true
 
@@ -89,7 +104,6 @@ func step():
 		print("step: unknown op [",op,"=", ram[ip-1],"] at ram[",ip-1,"]")
 		return false
 	return true
-
 
 func _init():
 	ram.resize(1024)
